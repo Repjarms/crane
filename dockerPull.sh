07dae574063afa91b42c8cf84ea56f35c685fc1c
repjarms <jeffarn12@gmail.com>
2017-12-26 13:51:31 -0500

    loop through all containers on image and determine proxy

--- dockerPull.sh
+++ dockerPull.sh
@@ -7,25 +7,32 @@ REPO_NAME=$1
 echo $REPO_NAME
 
 
-# get container id
-CONTAINER_ID=$(docker ps -a -q --filter ancestor=$REPO_NAME)
-echo "Found existing container at $CONTAINER_ID"
+# loop through all containers spawned from the related image and stop/remove
+# them
 
-# stop existing container
-echo "stopping existing container $CONTAINER_ID"
-echo "docker stop $CONTAINER_ID" | bash
+echo "Finding all containers running current image"
 
-# remove existing container
-echo "removing existing container $CONTAINER_ID"
-echo "docker rm $CONTAINER_ID" | bash
+for con in $(docker ps -a -q --filter ancestor=$REPO_NAME); do
+  # stop current container
+  echo "stopping existing container $con"
+  echo "docker stop $con" | bash
+
+  # remove current container
+  echo "removing existing container $con"
+  echo "docker rm $con" | bash
+done
 
 # remove existing image
-echo "docker rmi $1" | bash
+#echo "docker rmi $1" | bash
 
 # pull latest image, defaults to latest tag
 echo "docker pull $1" | bash
 
-# run new container using latest image
-echo "docker run -d -p 8012:8012 $1" | bash
+# get exposed port from the docker config
+APPLICATION_PORT=$(docker inspect --format='{{.Config.ExposedPorts}}' $1 | tr -dc 0-9)
+echo "application exposed port $APPLICATION_PORT"
 
-# proxy the ip address to something that is reachable
+# run new container using latest image
+echo "docker run -d -p $APPLICATION_PORT:$APPLICATION_PORT $1" | bash
+echo "application running latest image of $1 on port $APPLICATION_PORT"
+
